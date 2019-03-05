c403951348200bc6238a74a93bc0a4f8522ce9cc
elitan <johan@eliasson.me>
2019-03-05 09:18:52 +0100

    naming on HASURA env variables

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 exports.USER_FIELDS = process.env.USER_FIELDS ? process.env.USER_FIELDS.split(',') : [];
-exports.HASURA_GRAPHQL_ENDPOINT = process.env.HASURA_GRAPHQL_ENDPOINT || 'https://hasura.your-app.com/v1alpha1/graphql';
-exports.HASURA_GRAPHQL_ACCESS_KEY = process.env.HASURA_GRAPHQL_ACCESS_KEY || 'hasura-access-key';
-exports.HASURA_GRAPHQL_JWT_SECRET = process.env.HASURA_GRAPHQL_JWT_SECRET ? JSON.parse(process.env.HASURA_GRAPHQL_JWT_SECRET) : {'type':'HS256', 'key': 'secretkey'};
+exports.HASURA_GQE_ENDPOINT = process.env.HASURA_GQE_ENDPOINT || 'https://hasura.your-app.com/v1alpha1/graphql';
+exports.HASURA_GQE_ADMIN_SECRET = process.env.HASURA_GQE_ADMIN_SECRET || 'hasura-admin-secret';
+exports.HASURA_GQE_JWT_SECRET = process.env.HASURA_GQE_JWT_SECRET ? JSON.parse(process.env.HASURA_GRAPHQL_JWT_SECRET) : {'type':'HS256', 'key': 'secretkey'};
 exports.S3_ACCESS_KEY_ID = process.env.S3_ACCESS_KEY_ID || 's3-access-key-id';
 exports.S3_SECRET_ACCESS_KEY = process.env.S3_SECRET_ACCESS_KEY || 's3-secret-access-key';
 exports.S3_ENDPOINT = process.env.S3_ENDPOINT || 's3-endpoint';
