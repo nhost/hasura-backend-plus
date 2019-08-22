e17b5f6435efc72ca0e408b4e551f567c9f6d9d3
Travis Reynolds <travis@cirruscreative.co.uk>
2019-08-22 15:05:26 +0100

    Add req to apollo ctx

--- src/graphql/graphql.js
+++ src/graphql/graphql.js
@@ -5,6 +5,7 @@ const resolvers = require('./schema/resolvers');
 const graphql = new ApolloServer({
   typeDefs,
   resolvers,
+  context: ({ req }) => ({ req }),
   playground: process.env.NODE_ENV !== 'production',
 });
 
