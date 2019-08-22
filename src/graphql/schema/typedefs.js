a26ff038628f4df5251b9d8b71f5dab9a20d389a
Travis Reynolds <travis@cirruscreative.co.uk>
2019-08-22 13:54:30 +0100

    Add all mutations

--- src/graphql/schema/typedefs.js
+++ src/graphql/schema/typedefs.js
@@ -1,12 +1,19 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+  scalar Json
+
   type Query {
     hello: String
   }
+
   type Mutation {
+    register(username: String!, password: String!, data: Json): Boolean!
+    activate(token: String!): Boolean!
     login(username: String!, password: String!): AuthPayload!
+    refetchToken(userId: String!, refetchToken: String!): AuthPayload!
   }
+
   type AuthPayload {
     jwtToken: String!
     refetchToken: String!
