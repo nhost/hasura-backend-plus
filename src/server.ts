1e6375e5fad8f1e91245eee170380c4ebd77475f
Pilou <24897252+plmercereau@users.noreply.github.com>
2020-03-21 13:51:54 +0100

    test: close Express once all the tests are done (#128)

--- src/server.ts
+++ src/server.ts
@@ -10,34 +10,29 @@ import { json } from 'body-parser'
 import { limiter } from './limiter'
 import { router } from './routes'
 
-export const app = express()
+const app = express()
 
-try {
-  if (process.env.NODE_ENV === 'production') {
-    app.use(limiter)
-  }
-
-  app.use(helmet())
-  app.use(json())
-  app.use(cors())
-  app.use(fileUpload())
-
-  /**
-   * Set a cookie secret to enable server validation of cookies.
-   */
-  if (COOKIE_SECRET) {
-    app.use(cookie(COOKIE_SECRET))
-  } else {
-    app.use(cookie())
-  }
-
-  app.use(router)
-  app.use(errors)
-
-  app.listen(SERVER_PORT, () => {
-    console.log(`Running on http://localhost:${SERVER_PORT}`)
-  })
-} catch (err) {
-  console.error(err)
-  process.exit(1)
+if (process.env.NODE_ENV === 'production') {
+  app.use(limiter)
 }
+
+app.use(helmet())
+app.use(json())
+app.use(cors())
+app.use(fileUpload())
+
+/**
+ * Set a cookie secret to enable server validation of cookies.
+ */
+if (COOKIE_SECRET) {
+  app.use(cookie(COOKIE_SECRET))
+} else {
+  app.use(cookie())
+}
+
+app.use(router)
+app.use(errors)
+
+export const server = app.listen(SERVER_PORT, () => {
+  console.log(`Running on http://localhost:${SERVER_PORT}`)
+})
