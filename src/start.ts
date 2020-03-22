c29ceeabdbe37e154015b6c08838db20b7e90536
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2020-03-22 22:41:34 +0100

    fix: correct imports

--- src/start.ts
+++ src/start.ts
@@ -1,5 +1,5 @@
-import { app } from 'server'
-import { SERVER_PORT } from 'shared/config'
+import { app } from './server'
+import { SERVER_PORT } from '@shared/config'
 
 export const server = app.listen(SERVER_PORT, () => {
   console.log(`Running on http://localhost:${SERVER_PORT}`)
