361c338ffc8fccdfe7b179d577fa97360a0a0b0d
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2020-03-22 22:42:34 +0100

    test: full test cycle with docker-compose

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,11 @@
 FROM keymetrics/pm2:12-alpine
 
+ARG NODE_ENV=production
+ENV NODE_ENV $NODE_ENV
+
+# git is required for testing with jest --watch
+RUN if [ "$NODE_ENV" != "production" ]; then apk update && apk upgrade && apk add git; fi
+
 WORKDIR /app
 
 COPY package*.json ./
--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,32 @@
-#!/bin/sh
-# set -e
-docker-compose -f docker-compose.yaml -f docker-compose.test.yaml up -d
+#!/bin/bash
+set -e
+export HASURA_GRAPHQL_ADMIN_SECRET=$(cat .env.test | sed 's/#.*//g' | grep -o '^HASURA_GRAPHQL_ADMIN_SECRET.*' | xargs)
+# Create docker services
+docker-compose -p hbp_test -f docker-compose.yaml -f docker-compose.test.yaml up -d
+
+wait-for() {
+    until $(curl -X GET --output /dev/null --silent --head --fail ${1}); do
+        printf '.'
+        sleep 1
+    done
+    echo
+}
+
+{ # 'try' block
+    printf 'Waiting for Hasura Backend Plus to be ready...'
+    wait-for http://localhost:3000/healthz
+    printf 'Waiting for Hasura Graphql Engine to be ready...'
+    wait-for http://localhost:8080/healthz
+    docker exec -it hbp_test_hasura-backend-plus_1 yarn test
+} || { # 'catch' block
+    test_failed=true
+}
+
+# 'finally' block
+# Remove docker services
+docker-compose -p hbp_test down -v --remove-orphans
+
+if [ "$test_failed" = true ] ; then
+    echo 'Tests failed'
+    exit 1
+fi
