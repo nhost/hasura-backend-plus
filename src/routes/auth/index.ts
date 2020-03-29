0c7260294e7cdf369f68e875c2a253e4a32c514e
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2020-03-29 21:58:51 +0200

    test(storage): skeleton test file

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -1,13 +1,3 @@
-// export * from './mfa'
-// export * from './account'
-// export * from './token'
-// export * from './email'
-// export * from './password'
-
-// export { default as getJwks } from './jwks'
-// export { default as loginAccount } from './login'
-// export { default as registerAccount } from './register'
-
 import { Router } from 'express'
 
 import getJwks from './jwks'
