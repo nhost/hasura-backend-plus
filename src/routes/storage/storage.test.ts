e9570735d5510fb8c6784172276e7b6b71a0962e
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2020-03-31 11:13:27 +0200

    test(storage): storage jest tests

--- src/routes/storage/storage.test.ts
+++ src/routes/storage/storage.test.ts
@@ -1,9 +1,67 @@
 import 'jest-extended'
+import { v4 as uuidv4 } from 'uuid'
+import fs from 'fs'
+import { promisify } from 'util'
 
 import { account, request } from '@shared/test-mock-account'
+import { HASURA_GRAPHQL_ADMIN_SECRET } from '@shared/config'
+const readFile = promisify(fs.readFile)
+
+const fileKey = uuidv4()
+let fileToken: string
+let newFileToken: string
+const filePath = 'package.json'
 
 it('should upload a file', async () => {
-  const { status } = await request.post('/storage/upload')
-  console.log(`Upload test using the account ${account.email}`)
+  const { status, body } = await request
+    .post('/storage/upload')
+    .set('Authorization', `Bearer ${account.token}`)
+    .set('x-key', fileKey)
+    .attach('file', filePath)
+  fileToken = body.token
   expect(status).toEqual(200)
+})
+
+it('should get file metadata', async () => {
+  const {
+    status,
+    body: { filename, token, key }
+  } = await request
+    .get(`/storage/file-meta/${fileKey}`)
+    .set('Authorization', `Bearer ${account.token}`)
+  expect(status).toEqual(200)
+  expect(filename).toEqual(filePath)
+  expect(token).toEqual(fileToken)
+  expect(key).toEqual(fileKey)
+})
+
+it('should get file', async () => {
+  const { status, text } = await request
+    .get(`/storage/file/${fileKey}`)
+    .set('Authorization', `Bearer ${account.token}`)
+    .query({ token: fileToken })
+  const fileData = await readFile(filePath, 'utf8')
+  expect(status).toEqual(200)
+  expect(text).toEqual(fileData)
+})
+
+it('should revoke the file', async () => {
+  const { status: revokeStatus, body } = await request
+    .post(`/storage/revoke/${fileKey}`)
+    .set('x-hasura-admin-secret', HASURA_GRAPHQL_ADMIN_SECRET as string)
+  newFileToken = body.token
+  expect(revokeStatus).toEqual(200)
+  const { status: getFileStatus } = await request
+    .get(`/storage/file/${fileKey}`)
+    .set('Authorization', `Bearer ${account.token}`)
+    .query({ token: fileToken })
+  expect(getFileStatus).toEqual(403)
+})
+
+it('should delete file', async () => {
+  const { status } = await request
+    .delete(`/storage/file/${fileKey}`)
+    .set('Authorization', `Bearer ${account.token}`)
+    .query({ token: newFileToken })
+  expect(status).toEqual(204)
 })
