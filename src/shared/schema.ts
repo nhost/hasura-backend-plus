cbef8c0042fba7225171bb872ab8137ce07fd753
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2020-04-02 20:22:11 +0200

    fix(auth): split USER_FIELDS into two environment variables
    USER_CLAIMS_FIELDS for custom JWT Hasura claims, and USER_REGISTRATION_FIELDS for columns to be
    inserted upon registration

--- src/shared/schema.ts
+++ src/shared/schema.ts
@@ -1,4 +1,4 @@
-import { ALLOWED_EMAIL_DOMAINS, USER_FIELDS } from './config'
+import { ALLOWED_EMAIL_DOMAINS, USER_REGISTRATION_FIELDS } from './config'
 import Joi from '@hapi/joi'
 
 interface ExtendedStringSchema extends Joi.StringSchema {
@@ -48,14 +48,15 @@ const accountSchema = {
 export const registerSchema = Joi.object({
   ...accountSchema,
   user_data: Joi.object(
-    USER_FIELDS.reduce<{ [k: string]: Joi.Schema[] }>(
+    USER_REGISTRATION_FIELDS.reduce<{ [k: string]: Joi.Schema[] }>(
       (aggr, key) => ({
         ...aggr,
         [key]: [
           Joi.string(),
           Joi.number(),
           Joi.boolean(),
-          Joi.array().items(Joi.string(), Joi.number(), Joi.boolean())
+          Joi.object(),
+          Joi.array().items(Joi.string(), Joi.number(), Joi.boolean(), Joi.object())
         ]
       }),
       {}
