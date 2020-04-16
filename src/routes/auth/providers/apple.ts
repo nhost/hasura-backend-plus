99fbb43559944e08216111860ce53c99ee12373b
Weyert de Boer <weyert@innerfuse.biz>
2020-04-17 00:52:40 +0100

    chore: improve inline comments

--- src/routes/auth/providers/apple.ts
+++ src/routes/auth/providers/apple.ts
@@ -9,13 +9,11 @@ export default (router: Router): void => {
 
   // Checks if the strategy is enabled. Don't create any route otherwise
   if (options) {
-    // Convert contents from base64 string to string
+    // Convert contents from base64 string to string to avoid issues with line breaks in the environment variable
     const decodedPrivateKey = new Buffer(options.key, 'base64').toString('ascii')
-    console.log(`decodedPrivateKey: ${decodedPrivateKey}`)
 
     // Overwrite the `key`-option with the decoded private key supplied by Apple
     options.key = decodedPrivateKey
-    console.log(`All Apple strategy settings: `, options)
 
     if (!options.clientID || !options.teamID || !options.keyID || !options.key) {
       throw Boom.badImplementation(`Missing environment variables for Login with Apple OAuth.`)
