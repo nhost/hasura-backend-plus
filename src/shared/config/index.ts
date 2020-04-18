1d4e403bf234bfa4644bdb2faa908994a0832463
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2020-04-18 21:44:58 +0200

    fix: re-put dotenv init in config

--- src/shared/config/index.ts
+++ src/shared/config/index.ts
@@ -1,5 +1,13 @@
 import Boom from '@hapi/boom'
+import dotenv from 'dotenv'
 import { HASURA_ENDPOINT } from './application'
+
+// Load '.env' file if production mode, '.env.<NODE_ENV>' otherwise
+const envFile =
+  process.env.NODE_ENV && process.env.NODE_ENV !== 'production'
+    ? `.env.${process.env.NODE_ENV}`
+    : '.env'
+dotenv.config({ path: envFile })
 
 export * from './application'
 export * from './authentication'
