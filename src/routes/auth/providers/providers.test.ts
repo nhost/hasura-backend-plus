161270ae2883b0cee65d7a930bcf0e212828e830
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2020-04-23 12:07:00 +0200

    test(auth): e2e github auth

--- src/routes/auth/providers/providers.test.ts
+++ src/routes/auth/providers/providers.test.ts
@@ -1,32 +1,46 @@
 /* eslint-disable jest/no-standalone-expect */
 import 'jest-extended'
-// import { initAgent } from '@shared/test-utils'
+import { initAgent } from '@shared/test-utils'
 import { SERVER_URL, PROVIDER_SUCCESS_REDIRECT, PROVIDERS } from '@shared/config'
 import { agent } from 'supertest'
 import { server } from '../../../start'
 import { itif } from '@shared/test-utils'
 
-// test('Oauth routes should not exist when disabled', async () => {
-//   const agent = initAgent({ PROVIDERS: {} })
-//   const github = await agent.get('/auth/providers/github')
-//   expect(github.status).toEqual(404)
-//   const google = await agent.get('/auth/providers/google')
-//   expect(google.status).toEqual(404)
-// })
+it('Oauth routes should not exist when disabled', async () => {
+  const tempAgent = initAgent({ PROVIDERS: {} })
+  const github = await tempAgent.get('/auth/providers/github')
+  expect(github.status).toEqual(404)
+  const google = await tempAgent.get('/auth/providers/google')
+  expect(google.status).toEqual(404)
+})
 
-// test('Github Oauth should be configured correctly', async () => {
-//   expect(() => initAgent({ PROVIDERS: { github: { clientSecret: undefined } } })).toThrow(
-//     'Missing environment variables for GitHub OAuth.'
-//   )
-// })
+it('Github Oauth should be configured correctly', async () => {
+  expect(() => initAgent({ PROVIDERS: { github: { clientSecret: undefined } } })).toThrow(
+    'Missing environment variables for GitHub OAuth.'
+  )
+})
 
 // TODO test functions in ./utils.ts
-// ? end-to-end tests?
 agent(server) // * Create the SuperTest agent
 
 // * Code that is executed after any jest test file that imports test-utiles
 afterAll(async () => {
   await server.close()
+})
+
+itif(
+  !!PROVIDERS.github && !!process.env.TEST_GITHUB_USERNAME && !!process.env.TEST_GITHUB_PASSWORD
+)('should register from a GitHub account', async () => {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  jest.setTimeout(60000)
+  await page.goto(`${SERVER_URL}/auth/providers/github`)
+  await expect(page).toFill('#login_field', process.env.TEST_GITHUB_USERNAME as string)
+  await expect(page).toFill('#password', process.env.TEST_GITHUB_PASSWORD as string)
+  await expect(page).toClick('[name="commit"]')
+  await page.waitForNavigation({ waitUntil: 'networkidle0' })
+  // Wait for the Google Auth redirection
+  if (page.url() !== PROVIDER_SUCCESS_REDIRECT) await page.waitFor(4000)
+  expect(page.url()).toBe(PROVIDER_SUCCESS_REDIRECT)
 })
 
 itif(
