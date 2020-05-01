665168725e9e60219ac4134825ca68864eadd708
elitan <johan@eliasson.me>
2020-05-01 15:26:38 +0200

    allow to specify auth method via env var

--- src/shared/email.ts
+++ src/shared/email.ts
@@ -5,7 +5,8 @@ import {
   SMTP_PORT,
   SMTP_SECURE,
   SMTP_SENDER,
-  SMTP_USER
+  SMTP_USER,
+  SMTP_AUTH_METHOD
 } from '@shared/config'
 
 import Email from 'email-templates'
@@ -22,7 +23,8 @@ const transport = nodemailer.createTransport({
   auth: {
     pass: SMTP_PASS,
     user: SMTP_USER
-  }
+  },
+  authMethod: SMTP_AUTH_METHOD
 })
 
 /**
