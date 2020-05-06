90f362e816a5d5c4793fc3b6effbd057383bae3d
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2020-05-06 21:50:24 +0200

    fix(auth): aUTO_ACTIVATE_NEW_USERS defaults to true

--- src/shared/config/authentication/registration.ts
+++ src/shared/config/authentication/registration.ts
@@ -8,7 +8,7 @@ export const {
   DEFAULT_USER_ROLE = 'user',
   DEFAULT_ANONYMOUS_ROLE = 'anonymous'
 } = process.env
-export const AUTO_ACTIVATE_NEW_USERS = castBooleanEnv('AUTO_ACTIVATE_NEW_USERS')
+export const AUTO_ACTIVATE_NEW_USERS = castBooleanEnv('AUTO_ACTIVATE_NEW_USERS', true)
 export const HIBP_ENABLE = castBooleanEnv('HIBP_ENABLE')
 export const REGISTRATION_CUSTOM_FIELDS = castStringArrayEnv('REGISTRATION_CUSTOM_FIELDS')
 export const MIN_PASSWORD_LENGTH = castIntEnv('MIN_PASSWORD_LENGTH', 3)
