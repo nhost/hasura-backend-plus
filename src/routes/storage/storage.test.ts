98e845525a72de2a52a34d26658708b4988e0657
elitan <johan@eliasson.me>
2020-05-07 14:59:37 +0200

    updated storage tests

--- src/routes/storage/storage.test.ts
+++ src/routes/storage/storage.test.ts
@@ -12,14 +12,14 @@ const initialDescription = 'description of the file'
 const newDescription = 'new description'
 
 it('should upload a new file', async () => {
+  await request.post('/auth/login').send({ email: account.email, password: account.password })
   const {
     status,
     body: {
       Metadata: { token, description }
     }
   } = await request
-    .post(`/storage/user/${getUserId()}/${filePath}`)
-    .set('Authorization', `Bearer ${account.token}`)
+    .post(`/storage/o/user/${getUserId()}/${filePath}`)
     .query({ description: initialDescription })
     .attach('file', filePath)
   expect(status).toEqual(200)
@@ -30,20 +30,20 @@ it('should upload a new file', async () => {
 
 it('should update an existing file', async () => {
   const { status } = await request
-    .post(`/storage/user/${getUserId()}/${filePath}`)
+    .post(`/storage/o/user/${getUserId()}/${filePath}`)
     .attach('file', filePath)
   expect(status).toEqual(200)
 })
 
 it('should not update an hypothetical file of another hypothetical user', async () => {
   const { status } = await request
-    .post(`/storage/user/another-user/another-file`)
+    .post(`/storage/o/user/another-user/another-file`)
     .attach('file', filePath)
   expect(status).toEqual(403)
 })
 
 it('should get file', async () => {
-  const { status, text } = await request.get(`/storage/user/${getUserId()}/${filePath}`)
+  const { status, text } = await request.get(`/storage/o/user/${getUserId()}/${filePath}`)
   const fileData = await readFile(filePath, 'utf8')
   expect(status).toEqual(200)
   expect(text).toEqual(fileData)
@@ -60,7 +60,7 @@ describe('Tests as an unauthenticated user', () => {
 
   it('should get file from the token stored in the file metadata while unauthenticated', async () => {
     const { status, text } = await request
-      .get(`/storage/user/${getUserId()}/${filePath}`)
+      .get(`/storage/o/user/${getUserId()}/${filePath}`)
       .query({ token: fileToken })
     const fileData = await readFile(filePath, 'utf8')
     expect(status).toEqual(200)
@@ -68,7 +68,7 @@ describe('Tests as an unauthenticated user', () => {
   })
 
   it('should not get file without authentication nor token', async () => {
-    const { status } = await request.get(`/storage/user/${getUserId()}/${filePath}`)
+    const { status } = await request.get(`/storage/o/user/${getUserId()}/${filePath}`)
     expect(status).toEqual(403)
   })
   // TODO attempt to get the file from another authenticated user
@@ -76,7 +76,7 @@ describe('Tests as an unauthenticated user', () => {
 
 it(`should update an existing file's metadata`, async () => {
   const { status } = await request
-    .post(`/storage/meta/user/${getUserId()}/${filePath}`)
+    .post(`/storage/m/user/${getUserId()}/${filePath}`)
     .query({ description: newDescription })
   expect(status).toEqual(200)
 })
@@ -87,14 +87,14 @@ it('should get file metadata', async () => {
     body: {
       Metadata: { filename, description }
     }
-  } = await request.get(`/storage/meta/user/${getUserId()}/${filePath}`)
+  } = await request.get(`/storage/m/user/${getUserId()}/${filePath}`)
   expect(status).toEqual(200)
   expect(filename).toEqual(filePath)
   expect(description).toEqual(newDescription)
 })
 
 it('should get the headers of all the user files', async () => {
-  const { status, body } = await request.get(`/storage/meta/user/${getUserId()}/`)
+  const { status, body } = await request.get(`/storage/m/user/${getUserId()}/`)
   expect(status).toEqual(200)
   expect(body).toBeArrayOfSize(1)
 })
@@ -107,14 +107,14 @@ it('should get a zip that contains all user files', async () => {
 })
 
 it('should delete file metadata', async () => {
-  const { status } = await request.delete(`/storage/meta/user/${getUserId()}/${filePath}`)
+  const { status } = await request.delete(`/storage/m/user/${getUserId()}/${filePath}`)
   expect(status).toEqual(204)
   const {
     status: getStatus,
     body: {
       Metadata: { filename, token, description }
     }
-  } = await request.get(`/storage/meta/user/${getUserId()}/${filePath}`)
+  } = await request.get(`/storage/m/user/${getUserId()}/${filePath}`)
   expect(getStatus).toEqual(200)
   expect(filename).toBeUndefined()
   expect(token).toBeString()
