886116a3dc4f5b42782de4a935d5ec4b9d6ebfce
elitan <johan@eliasson.me>
2020-05-11 09:38:37 +0200

    default false for auto migration

--- src/shared/config/application.ts
+++ src/shared/config/application.ts
@@ -15,7 +15,7 @@ export const HASURA_ENDPOINT = process.env.HASURA_ENDPOINT as string
 const autoMigrateSettings = (envVal?: string): boolean | string => {
   if (envVal?.toLowerCase() === 'false') return false
   if (envVal?.toLowerCase() === 'v1') return 'v1'
-  return true
+  return false
 }
 export const AUTO_MIGRATE = autoMigrateSettings(process.env.AUTO_MIGRATE)
 /**
