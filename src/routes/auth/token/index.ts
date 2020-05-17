b22452243e183b9109382f77398983ca9a88fcc9
Pilou <24897252+plmercereau@users.noreply.github.com>
2020-05-17 09:11:45 +0000

    Get token and docs (#236)
    * refactor(auth): change token endpoint from HTTP POST to GET
    
    * ci(docs): trigger publish docs when changes are done to the examples directory
    
    * docs(auth): login, mfa, jwt

--- src/routes/auth/token/index.ts
+++ src/routes/auth/token/index.ts
@@ -2,4 +2,4 @@ import { Router } from 'express'
 import refreshToken from './refresh'
 import revokeToken from './revoke'
 
-export default Router().post('/refresh', refreshToken).post('/revoke', revokeToken)
+export default Router().get('/refresh', refreshToken).post('/revoke', revokeToken)
