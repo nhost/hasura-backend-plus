8d711e0aec4cc8b884d7ea411eeb7e4c7ee0f001
Jacob King <jking@base16consulting.com>
2021-04-08 14:27:41 +0200

    Use JWT_KEY_FILE_PATH if available
    While the README had you set the JWT_KEY_FILE_PATH, it was being ignored
    when the code was loading.
    
    This fixes that to use the environment variable and fallback on a
    default path if needed.

--- src/shared/config/authentication/jwt.ts
+++ src/shared/config/authentication/jwt.ts
@@ -7,9 +7,9 @@ import { castIntEnv, castStringArrayEnv } from '../utils'
 export const {
   JWT_KEY,
   JWT_ALGORITHM = 'RS256',
-  JWT_CLAIMS_NAMESPACE = 'https://hasura.io/jwt/claims'
-} = process.env
-export const JWT_KEY_FILE_PATH = path.resolve(process.env.PWD || '.', 'custom/keys/private.pem')
+  JWT_CLAIMS_NAMESPACE = 'https://hasura.io/jwt/claims',
+  JWT_KEY_FILE_PATH = path.resolve(process.env.PWD || '.', 'custom/keys/private.pem'),
+} = process.env;
 export const JWT_EXPIRES_IN = castIntEnv('JWT_EXPIRES_IN', 15)
 export const JWT_REFRESH_EXPIRES_IN = castIntEnv('JWT_REFRESH_EXPIRES_IN', 43200)
 export const JWT_CUSTOM_FIELDS = castStringArrayEnv('JWT_CUSTOM_FIELDS')
