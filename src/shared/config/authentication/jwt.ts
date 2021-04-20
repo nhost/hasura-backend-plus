030ed41b987bc902f3231a30dbc24859e9a15209
Ziad Beyens <ziyad.beyens@gmail.com>
2021-04-20 22:17:28 +0200

    fix: support \n
    Replace `\n` by an actual line break.

--- src/shared/config/authentication/jwt.ts
+++ src/shared/config/authentication/jwt.ts
@@ -6,7 +6,7 @@ import { castIntEnv, castStringArrayEnv } from '../utils'
  */
 export const JWT = {
   get KEY() {
-    return process.env.JWT_KEY || ''
+    return process.env.JWT_KEY?.replace(/\\n/g, '\n') || ''
   },
   get ALGORITHM() {
     return process.env.JWT_ALGORITHM || 'RS256'
