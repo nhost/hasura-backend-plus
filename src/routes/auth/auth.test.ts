4cb87438ee44f72d20ab1853df26f47ba7d38383
Nuno Pato <nunopato@gmail.com>
2021-05-11 17:54:12 +0200

    Fix remaining tests

--- src/routes/auth/auth.test.ts
+++ src/routes/auth/auth.test.ts
@@ -2,7 +2,15 @@ import 'jest-extended'
 import { v4 as uuidv4 } from 'uuid'
 
 import { APPLICATION, JWT as CONFIG_JWT, REGISTRATION, HEADERS } from '@shared/config'
-import { generateRandomEmail, generateRandomString, deleteMailHogEmail, mailHogSearch, registerAccount, registerAndLoginAccount, getHeaderFromLatestEmailAndDelete } from '@test/utils'
+import {
+  generateRandomEmail,
+  generateRandomString,
+  deleteMailHogEmail,
+  mailHogSearch,
+  registerAccount,
+  registerAndLoginAccount,
+  getHeaderFromLatestEmailAndDelete
+} from '@test/utils'
 
 import { JWT } from 'jose'
 import { Token } from '@shared/types'
@@ -22,7 +30,8 @@ function errorMessageEqual(msg: string) {
 
 const pwndPasswordIt = REGISTRATION.HIBP_ENABLE ? it : it.skip
 pwndPasswordIt('should tell the password has been pwned', (done) => {
-  request.post('/auth/register')
+  request
+    .post('/auth/register')
     .send({ email: generateRandomEmail(), password: '123456' })
     .expect(400)
     .expect(errorMessageEqual('Password is too weak.'))
@@ -32,52 +41,65 @@ pwndPasswordIt('should tell the password has been pwned', (done) => {
 it('should create an account', (done) => {
   request
     .post('/auth/register')
-    .send({ email: generateRandomEmail(), password: generateRandomString(), user_data: { name: 'Test name' } })
+    .send({
+      email: generateRandomEmail(),
+      password: generateRandomString(),
+      user_data: { name: 'Test name' }
+    })
     .expect(200)
     .end(end(done))
 })
 
 it('should create an account without a password when magic link login is enabled', async () => {
-  await withEnv({
-    ENABLE_MAGIC_LINK: 'true'
-  }, request, async () => {
-    const email = generateRandomEmail()
-
-    const { body, status } = await request
-      .post('/auth/register')
-      .send({ email, user_data: { name: 'Test name' } })
-
-    expect(status).toEqual(200)
-    expect(body.jwt_token).toBeNull()
-    expect(body.jwt_expires_in).toBeNull()
-    expect(body.user).toBeTruthy()
-
-    const [message] = await mailHogSearch(email)
-    expect(message).toBeTruthy()
-    const token = message.Content.Headers['X-Token'][0]
-    await deleteMailHogEmail(message)
-
+  await withEnv(
     {
-      const { status } = await request.get(`/auth/magic-link?action=sign-up&token=${token}`)
-      expect(status).toBe(302)
+      ENABLE_MAGIC_LINK: 'true'
+    },
+    request,
+    async () => {
+      const email = generateRandomEmail()
+
+      const { body, status } = await request
+        .post('/auth/register')
+        .send({ email, user_data: { name: 'Test name' } })
+
+      expect(status).toEqual(200)
+      expect(body.jwt_token).toBeNull()
+      expect(body.jwt_expires_in).toBeNull()
+      expect(body.user).toBeTruthy()
+
+      const [message] = await mailHogSearch(email)
+      expect(message).toBeTruthy()
+      const token = message.Content.Headers['X-Token'][0]
+      await deleteMailHogEmail(message)
+
+      {
+        const { status } = await request.get(`/auth/magic-link?action=sign-up&token=${token}`)
+        expect(status).toBe(302)
+      }
     }
-  })
+  )
 })
 
 it('should not create an account without a password when magic link login is disabled', (done) => {
-  withEnv({
-    ENABLE_MAGIC_LINK: 'false'
-  }, request, async () => {
-    request
-      .post('/auth/register')
-      .send({ email: generateRandomEmail(), user_data: { name: 'Test name' } })
-      .expect(400)
-      .end(end(done))
-  })
+  withEnv(
+    {
+      ENABLE_MAGIC_LINK: 'false'
+    },
+    request,
+    async () => {
+      request
+        .post('/auth/register')
+        .send({ email: generateRandomEmail(), user_data: { name: 'Test name' } })
+        .expect(400)
+        .end(end(done))
+    }
+  )
 })
 
 it('should fail to create account with unallowed role', (done) => {
-  request.post('/auth/register')
+  request
+    .post('/auth/register')
     .send({
       email: generateRandomEmail(),
       password: generateRandomString(),
@@ -91,7 +113,8 @@ it('should fail to create account with unallowed role', (done) => {
 })
 
 it('should fail to create account with default_role that does not overlap allowed_roles', (done) => {
-  request.post('/auth/register')
+  request
+    .post('/auth/register')
     .send({
       email: generateRandomEmail(),
       password: generateRandomString(),
@@ -106,7 +129,8 @@ it('should fail to create account with default_role that does not overlap allowe
 })
 
 it('should create account with default_role that is in the ALLOWED_USER_ROLES variable', (done) => {
-  request.post('/auth/register')
+  request
+    .post('/auth/register')
     .send({
       email: generateRandomEmail(),
       password: generateRandomString(),
@@ -120,7 +144,8 @@ it('should create account with default_role that is in the ALLOWED_USER_ROLES va
 })
 
 it('should register account with default_role and allowed_roles set', (done) => {
-  request.post('/auth/register')
+  request
+    .post('/auth/register')
     .send({
       email: generateRandomEmail(),
       password: generateRandomString(),
@@ -139,11 +164,13 @@ it('should tell the account already exists', (done) => {
   const password = generateRandomString()
 
   request
-    .post('/auth/register').send({ email, password })
+    .post('/auth/register')
+    .send({ email, password })
     .end((err) => {
-      if(err) return done(err)
+      if (err) return done(err)
       request
-        .post('/auth/register').send({ email, password })
+        .post('/auth/register')
+        .send({ email, password })
         .expect(400)
         .expect(errorMessageEqual('Account already exists.'))
         .end(end(done))
@@ -151,39 +178,48 @@ it('should tell the account already exists', (done) => {
 })
 
 it('should fail to activate an user from a wrong ticket', async () => {
-  await withEnv({
-    AUTO_ACTIVATE_NEW_USERS: 'false',
-    EMAILS_ENABLE: 'true'
-  }, request, async () => {
-    const { status, redirect, header } = await request.get(`/auth/activate?ticket=${uuidv4()}`)
-    expect(
-      status === 500 || (status === 302 && redirect && header?.location === APPLICATION.REDIRECT_URL_ERROR)
-    ).toBeTrue()
-  })
+  await withEnv(
+    {
+      AUTO_ACTIVATE_NEW_USERS: 'false',
+      EMAILS_ENABLE: 'true'
+    },
+    request,
+    async () => {
+      const { status, redirect, header } = await request.get(`/auth/activate?ticket=${uuidv4()}`)
+      expect(
+        status === 500 ||
+          (status === 302 && redirect && header?.location === APPLICATION.REDIRECT_URL_ERROR)
+      ).toBeTrue()
+    }
+  )
 })
 
 it('should activate the account from a valid ticket', async () => {
-  await withEnv({
-    AUTO_ACTIVATE_NEW_USERS: 'false',
-    EMAILS_ENABLE: 'true'
-  }, request, async () => {
-    const email = generateRandomEmail()
-    const password = generateRandomString()
+  await withEnv(
+    {
+      AUTO_ACTIVATE_NEW_USERS: 'false',
+      EMAILS_ENABLE: 'true'
+    },
+    request,
+    async () => {
+      const email = generateRandomEmail()
+      const password = generateRandomString()
 
-    await request.post('/auth/register').send({ email, password })
+      await request.post('/auth/register').send({ email, password })
 
-    const ticket = await getHeaderFromLatestEmailAndDelete(email, 'X-Ticket')
+      const ticket = await getHeaderFromLatestEmailAndDelete(email, 'X-Ticket')
 
-    const { status } = await request.get(`/auth/activate?ticket=${ticket}`)
-    expect(status).toBeOneOf([204, 302])
-  })
+      const { status } = await request.get(`/auth/activate?ticket=${ticket}`)
+      expect(status).toBeOneOf([204, 302])
+    }
+  )
 })
 
 it('should not sign user with wrong password', (done) => {
-  registerAccount(request).then(({ email, password}) => {
+  registerAccount(request).then(({ email, password }) => {
     request
       .post('/auth/login')
-      .send({email, password: password + '1'})
+      .send({ email, password: password + '1' })
       .expect(401)
       .end(end(done))
   })
@@ -206,8 +242,9 @@ it('should complain about incorrect email', (done) => {
 })
 
 it('should sign the user in', (done) => {
-  registerAccount(request).then(({ email, password}) => {
-    request.post('/auth/login')
+  registerAccount(request).then(({ email, password }) => {
+    request
+      .post('/auth/login')
       .send({ email, password })
       .expect(validJwt())
       .expect(200)
@@ -216,54 +253,58 @@ it('should sign the user in', (done) => {
 })
 
 it('should sign the user in without password when magic link is enabled', async () => {
-  await withEnv({
-    ENABLE_MAGIC_LINK: 'true',
-    AUTO_ACTIVATE_NEW_USERS: 'false',
-    VERIFY_EMAILS: 'true'
-  }, request, async () => {
-    const email = generateRandomEmail();
-
-    const { body, status } = await request
-      .post('/auth/register')
-      .send({ email, user_data: { name: 'Test name' } })
-
-    expect(status).toEqual(200)
-    expect(body.jwt_token).toBeNull()
-    expect(body.jwt_expires_in).toBeNull()
-    expect(body.user).toBeTruthy()
-
-    const token = await getHeaderFromLatestEmailAndDelete(email, 'X-Token')
-
+  await withEnv(
     {
-      const { status } = await request.get(`/auth/magic-link?action=sign-up&token=${token}`)
-      expect(status).toBe(302)
-    }
+      ENABLE_MAGIC_LINK: 'true',
+      AUTO_ACTIVATE_NEW_USERS: 'false',
+      VERIFY_EMAILS: 'true'
+    },
+    request,
+    async () => {
+      const email = generateRandomEmail()
 
-    {
-      const { body, status } = await request.post('/auth/login').send({ email })
+      const { body, status } = await request
+        .post('/auth/register')
+        .send({ email, user_data: { name: 'Test name' } })
+
       expect(status).toEqual(200)
-      expect(body.magicLink).toBeTrue()
+      expect(body.jwt_token).toBeNull()
+      expect(body.jwt_expires_in).toBeNull()
+      expect(body.user).toBeTruthy()
 
       const token = await getHeaderFromLatestEmailAndDelete(email, 'X-Token')
 
       {
-        const { status } = await request.get(`/auth/magic-link?action=log-in&token=${token}`)
+        const { status } = await request.get(`/auth/magic-link?action=sign-up&token=${token}`)
         expect(status).toBe(302)
       }
+
+      {
+        const { body, status } = await request.post('/auth/login').send({ email })
+        expect(status).toEqual(200)
+        expect(body.magicLink).toBeTrue()
+
+        const token = await getHeaderFromLatestEmailAndDelete(email, 'X-Token')
+
+        {
+          const { status } = await request.get(`/auth/magic-link?action=log-in&token=${token}`)
+          expect(status).toBe(302)
+        }
+      }
     }
-  })
+  )
 })
 
 it('should not sign the user in without password when magic link is disabled', (done) => {
-  withEnv({
-    ENABLE_MAGIC_LINK: 'false'
-  }, request, async () => {
-    request
-      .post('/auth/login')
-      .send({ email: generateRandomEmail() })
-      .expect(400)
-      .end(end(done))
-  });
+  withEnv(
+    {
+      ENABLE_MAGIC_LINK: 'false'
+    },
+    request,
+    async () => {
+      request.post('/auth/login').send({ email: generateRandomEmail() }).expect(400).end(end(done))
+    }
+  )
 })
 
 it('should not sign user in with invalid admin secret', (done) => {
@@ -274,19 +315,19 @@ it('should not sign user in with invalid admin secret', (done) => {
       .send({ email, password: 'invalidpassword' })
       .expect(401)
       .end(end(done))
-  });
+  })
 })
 
 it('should sign in user with valid admin secret', (done) => {
-  registerAccount(request).then(({ email }) => {
+  registerAccount(request).then(({ email, password }) => {
     request
       .post('/auth/login')
       .set(HEADERS.ADMIN_SECRET_HEADER, APPLICATION.HASURA_GRAPHQL_ADMIN_SECRET as string)
-      .send({ email, password: 'invalidpassword' })
+      .send({ email, password })
       .expect(200)
       .expect(validJwt())
       .end(end(done))
-  });
+  })
 })
 
 it('should decode a valid custom user claim', (done) => {
@@ -297,26 +338,23 @@ it('should decode a valid custom user claim', (done) => {
       .post('/auth/login')
       .send({ email, password })
       .expect(200)
-      .expect(saveJwt(j => jwtToken = j))
+      .expect(saveJwt((j) => (jwtToken = j)))
       .end((err) => {
-        if(err) return done(err)
+        if (err) return done(err)
 
         const decodedJwt = JWT.decode(jwtToken) as Token
         expect(decodedJwt[CONFIG_JWT.CLAIMS_NAMESPACE]).toBeObject()
         // Test if the custom claims work
         expect(decodedJwt[CONFIG_JWT.CLAIMS_NAMESPACE]['x-hasura-name']).toEqual('Test name')
-        done();
+        done()
       })
   })
 })
 
 it('should logout', (done) => {
   registerAndLoginAccount(request).then(() => {
-    request.post('/auth/logout')
-      .send()
-      .expect(204)
-      .end(end(done))
-  });
+    request.post('/auth/logout').send().expect(204).end(end(done))
+  })
 })
 
 describe('Tests without cookies', () => {
@@ -328,39 +366,36 @@ describe('Tests without cookies', () => {
         .expect(validJwt())
         .expect(validRefreshToken())
         .end(end(done))
-    });
+    })
   })
 
   it('should decode a valid custom user claim', (done) => {
     let jwtToken = ''
 
-    registerAccount(request, { name: 'Test name' }).then(({email, password}) => {
-        request
-          .post('/auth/login')
-          .send({ email, password, cookie: false })
-          .expect(validJwt())
-          .expect(validRefreshToken())
-          .expect(saveJwt((j) => jwtToken = j))
-          .end((err) => {
-            if(err) return done(err)
+    registerAccount(request, { name: 'Test name' }).then(({ email, password }) => {
+      request
+        .post('/auth/login')
+        .send({ email, password, cookie: false })
+        .expect(validJwt())
+        .expect(validRefreshToken())
+        .expect(saveJwt((j) => (jwtToken = j)))
+        .end((err) => {
+          if (err) return done(err)
 
-            const decodedJwt = JWT.decode(jwtToken) as Token
-            expect(decodedJwt[CONFIG_JWT.CLAIMS_NAMESPACE]).toBeObject()
-            // Test if the custom claims work
-            expect(decodedJwt[CONFIG_JWT.CLAIMS_NAMESPACE]['x-hasura-name']).toEqual('Test name')
+          const decodedJwt = JWT.decode(jwtToken) as Token
+          expect(decodedJwt[CONFIG_JWT.CLAIMS_NAMESPACE]).toBeObject()
+          // Test if the custom claims work
+          expect(decodedJwt[CONFIG_JWT.CLAIMS_NAMESPACE]['x-hasura-name']).toEqual('Test name')
 
-            done()
-          })
-      });
+          done()
+        })
+    })
   })
 })
 
 it('should delete an account', (done) => {
   registerAndLoginAccount(request).then(() => {
-    request
-      .post('/auth/delete')
-      .expect(204)
-      .end(end(done))
+    request.post('/auth/delete').expect(204).end(end(done))
   })
 })
 
