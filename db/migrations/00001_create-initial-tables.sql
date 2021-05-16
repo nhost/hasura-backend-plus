82889009a82add3fb66d84675aedd3db245623f8
Nuno Pato <nunopato@gmail.com>
2021-05-16 14:33:16 +0200

    Make initial SQL idempotent

--- db/migrations/00001_create-initial-tables.sql
+++ db/migrations/00001_create-initial-tables.sql
@@ -1,4 +1,4 @@
-CREATE TABLE auth.account_providers (
+CREATE TABLE IF NOT EXISTS auth.account_providers (
     id uuid DEFAULT public.gen_random_uuid() NOT NULL,
     created_at timestamp with time zone DEFAULT now() NOT NULL,
     updated_at timestamp with time zone DEFAULT now() NOT NULL,
@@ -7,14 +7,14 @@ CREATE TABLE auth.account_providers (
     auth_provider_unique_id text NOT NULL
 );
 
-CREATE TABLE auth.account_roles (
+CREATE TABLE IF NOT EXISTS auth.account_roles (
     id uuid DEFAULT public.gen_random_uuid() NOT NULL,
     created_at timestamp with time zone DEFAULT now() NOT NULL,
     account_id uuid NOT NULL,
     role text NOT NULL
 );
 
-CREATE TABLE auth.accounts (
+CREATE TABLE IF NOT EXISTS auth.accounts (
     id uuid DEFAULT public.gen_random_uuid() NOT NULL,
     created_at timestamp with time zone DEFAULT now() NOT NULL,
     updated_at timestamp with time zone DEFAULT now() NOT NULL,
@@ -34,22 +34,22 @@ CREATE TABLE auth.accounts (
     CONSTRAINT proper_new_email CHECK ((new_email OPERATOR(public.~*) '^[A-Za-z0-9._+%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$'::public.citext))
 );
 
-CREATE TABLE auth.providers (
+CREATE TABLE IF NOT EXISTS auth.providers (
     provider text NOT NULL
 );
 
-CREATE TABLE auth.refresh_tokens (
+CREATE TABLE IF NOT EXISTS auth.refresh_tokens (
     refresh_token uuid NOT NULL,
     created_at timestamp with time zone DEFAULT now() NOT NULL,
     expires_at timestamp with time zone NOT NULL,
     account_id uuid NOT NULL
 );
 
-CREATE TABLE auth.roles (
+CREATE TABLE IF NOT EXISTS auth.roles (
     role text NOT NULL
 );
 
-CREATE TABLE public.users (
+CREATE TABLE IF NOT EXISTS public.users (
     id uuid DEFAULT public.gen_random_uuid() NOT NULL,
     created_at timestamp with time zone DEFAULT now() NOT NULL,
     updated_at timestamp with time zone DEFAULT now() NOT NULL,
@@ -57,74 +57,53 @@ CREATE TABLE public.users (
     avatar_url text
 );
 
-ALTER TABLE ONLY auth.account_providers
-    ADD CONSTRAINT account_providers_account_id_auth_provider_key UNIQUE (account_id, auth_provider);
+CREATE OR REPLACE FUNCTION create_constraint_if_not_exists (t_name text, c_name text, constraint_sql text)
+  RETURNS void
+AS
+$BODY$
+  BEGIN
+    -- Look for our constraint
+    IF NOT EXISTS (SELECT constraint_name
+                   FROM information_schema.constraint_column_usage
+                   WHERE constraint_name = c_name) THEN
+        EXECUTE 'ALTER TABLE ' || t_name || ' ADD CONSTRAINT ' || c_name || ' ' || constraint_sql;
+    END IF;
+  END;
+$BODY$
+LANGUAGE plpgsql VOLATILE;
 
-ALTER TABLE ONLY auth.account_providers
-    ADD CONSTRAINT account_providers_auth_provider_auth_provider_unique_id_key UNIQUE (auth_provider, auth_provider_unique_id);
+SELECT create_constraint_if_not_exists('auth.account_providers', 'account_providers_account_id_auth_provider_key', 'UNIQUE (account_id, auth_provider);');
+SELECT create_constraint_if_not_exists('auth.account_providers', 'account_providers_auth_provider_auth_provider_unique_id_key', 'UNIQUE (auth_provider, auth_provider_unique_id);');
+SELECT create_constraint_if_not_exists('auth.account_providers', 'account_providers_pkey', 'PRIMARY KEY (id);');
+SELECT create_constraint_if_not_exists('auth.account_roles', 'account_roles_pkey', 'PRIMARY KEY (id);');
+SELECT create_constraint_if_not_exists('auth.accounts', 'accounts_email_key', 'UNIQUE (email);');
+SELECT create_constraint_if_not_exists('auth.accounts', 'accounts_new_email_key', 'UNIQUE (new_email);');
+SELECT create_constraint_if_not_exists('auth.accounts', 'accounts_pkey', 'PRIMARY KEY (id);');
+SELECT create_constraint_if_not_exists('auth.accounts', 'accounts_user_id_key', 'UNIQUE (user_id);');
+SELECT create_constraint_if_not_exists('auth.providers', 'providers_pkey', 'PRIMARY KEY (provider);');
+SELECT create_constraint_if_not_exists('auth.refresh_tokens', 'refresh_tokens_pkey', 'PRIMARY KEY (refresh_token);');
+SELECT create_constraint_if_not_exists('auth.roles', 'roles_pkey', 'PRIMARY KEY (role);');
+SELECT create_constraint_if_not_exists('auth.account_roles', 'user_roles_account_id_role_key', 'UNIQUE (account_id, role);');
+SELECT create_constraint_if_not_exists('public.users', 'users_pkey', 'PRIMARY KEY (id);');
+SELECT create_constraint_if_not_exists('auth.account_providers', 'account_providers_account_id_fkey', 'FOREIGN KEY (account_id) REFERENCES auth.accounts(id) ON UPDATE CASCADE ON DELETE CASCADE;');
+SELECT create_constraint_if_not_exists('auth.account_providers', 'account_providers_auth_provider_fkey', 'FOREIGN KEY (auth_provider) REFERENCES auth.providers(provider) ON UPDATE RESTRICT ON DELETE RESTRICT;');
+SELECT create_constraint_if_not_exists('auth.account_roles', 'account_roles_account_id_fkey', 'FOREIGN KEY (account_id) REFERENCES auth.accounts(id) ON UPDATE CASCADE ON DELETE CASCADE;');
+SELECT create_constraint_if_not_exists('auth.account_roles', 'account_roles_role_fkey', 'FOREIGN KEY (role) REFERENCES auth.roles(role) ON UPDATE CASCADE ON DELETE RESTRICT;');
+SELECT create_constraint_if_not_exists('auth.accounts', 'accounts_default_role_fkey', 'FOREIGN KEY (default_role) REFERENCES auth.roles(role) ON UPDATE CASCADE ON DELETE RESTRICT;');
+SELECT create_constraint_if_not_exists('auth.accounts', 'accounts_user_id_fkey', 'FOREIGN KEY (user_id) REFERENCES public.users(id) ON UPDATE CASCADE ON DELETE CASCADE;');
+SELECT create_constraint_if_not_exists('auth.refresh_tokens', 'refresh_tokens_account_id_fkey', 'FOREIGN KEY (account_id) REFERENCES auth.accounts(id) ON UPDATE CASCADE ON DELETE CASCADE;');
 
-ALTER TABLE ONLY auth.account_providers
-    ADD CONSTRAINT account_providers_pkey PRIMARY KEY (id);
-
-ALTER TABLE ONLY auth.account_roles
-    ADD CONSTRAINT account_roles_pkey PRIMARY KEY (id);
-
-ALTER TABLE ONLY auth.accounts
-    ADD CONSTRAINT accounts_email_key UNIQUE (email);
-
-ALTER TABLE ONLY auth.accounts
-    ADD CONSTRAINT accounts_new_email_key UNIQUE (new_email);
-
-ALTER TABLE ONLY auth.accounts
-    ADD CONSTRAINT accounts_pkey PRIMARY KEY (id);
-
-ALTER TABLE ONLY auth.accounts
-    ADD CONSTRAINT accounts_user_id_key UNIQUE (user_id);
-
-ALTER TABLE ONLY auth.providers
-    ADD CONSTRAINT providers_pkey PRIMARY KEY (provider);
-
-ALTER TABLE ONLY auth.refresh_tokens
-    ADD CONSTRAINT refresh_tokens_pkey PRIMARY KEY (refresh_token);
-
-ALTER TABLE ONLY auth.roles
-    ADD CONSTRAINT roles_pkey PRIMARY KEY (role);
-
-ALTER TABLE ONLY auth.account_roles
-    ADD CONSTRAINT user_roles_account_id_role_key UNIQUE (account_id, role);
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
-
+DROP TRIGGER IF EXISTS set_auth_account_providers_updated_at ON auth.account_providers;
 CREATE TRIGGER set_auth_account_providers_updated_at BEFORE UPDATE ON auth.account_providers FOR EACH ROW EXECUTE FUNCTION public.set_current_timestamp_updated_at();
 
+DROP TRIGGER IF EXISTS set_auth_accounts_updated_at ON auth.accounts;
 CREATE TRIGGER set_auth_accounts_updated_at BEFORE UPDATE ON auth.accounts FOR EACH ROW EXECUTE FUNCTION public.set_current_timestamp_updated_at();
 
+DROP TRIGGER IF EXISTS set_public_users_updated_at ON public.users;
 CREATE TRIGGER set_public_users_updated_at BEFORE UPDATE ON public.users FOR EACH ROW EXECUTE FUNCTION public.set_current_timestamp_updated_at();
 
-ALTER TABLE ONLY auth.account_providers
-    ADD CONSTRAINT account_providers_account_id_fkey FOREIGN KEY (account_id) REFERENCES auth.accounts(id) ON UPDATE CASCADE ON DELETE CASCADE;
-
-ALTER TABLE ONLY auth.account_providers
-    ADD CONSTRAINT account_providers_auth_provider_fkey FOREIGN KEY (auth_provider) REFERENCES auth.providers(provider) ON UPDATE RESTRICT ON DELETE RESTRICT;
-
-ALTER TABLE ONLY auth.account_roles
-    ADD CONSTRAINT account_roles_account_id_fkey FOREIGN KEY (account_id) REFERENCES auth.accounts(id) ON UPDATE CASCADE ON DELETE CASCADE;
-
-ALTER TABLE ONLY auth.account_roles
-    ADD CONSTRAINT account_roles_role_fkey FOREIGN KEY (role) REFERENCES auth.roles(role) ON UPDATE CASCADE ON DELETE RESTRICT;
-
-ALTER TABLE ONLY auth.accounts
-    ADD CONSTRAINT accounts_default_role_fkey FOREIGN KEY (default_role) REFERENCES auth.roles(role) ON UPDATE CASCADE ON DELETE RESTRICT;
-
-ALTER TABLE ONLY auth.accounts
-    ADD CONSTRAINT accounts_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON UPDATE CASCADE ON DELETE CASCADE;
-
-ALTER TABLE ONLY auth.refresh_tokens
-    ADD CONSTRAINT refresh_tokens_account_id_fkey FOREIGN KEY (account_id) REFERENCES auth.accounts(id) ON UPDATE CASCADE ON DELETE CASCADE;
-
 INSERT INTO auth.roles (role)
-    VALUES ('user'), ('anonymous'), ('me');
+    VALUES ('user'), ('anonymous'), ('me') ON CONFLICT DO NOTHING;
 
 INSERT INTO auth.providers (provider)
-    VALUES ('github'), ('facebook'), ('twitter'), ('google'), ('apple'),  ('linkedin'), ('windowslive'), ('spotify');
+    VALUES ('github'), ('facebook'), ('twitter'), ('google'), ('apple'),  ('linkedin'), ('windowslive'), ('spotify') ON CONFLICT DO NOTHING;
