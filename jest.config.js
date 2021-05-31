94baa6c27265526a00a99ca22e9a7d6d4bdcce8b
Nuno Pato <nunopato@gmail.com>
2021-05-31 17:23:32 +0200

    Remove global-setup since metadata is now handled programatically on start

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  globalSetup: '<rootDir>/src/test/global-setup.ts',
   verbose: true,
   moduleNameMapper: {
     '^@shared/(.*)$': '<rootDir>/src/shared/$1',
--- src/test/global-setup.ts
+++
@@ -1,8 +0,0 @@
-require('tsconfig-paths/register')
-import migrate from '../migrate'
-import { applyMigrations } from "../shared/migrations"
-
-export default async (): Promise<void> => {
-  await applyMigrations()
-  await migrate({ metadata: './db/hasura/metadata', migrations: './test-mocks/migrations' })
-}
