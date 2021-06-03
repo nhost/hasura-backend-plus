9bdf6dab28303a59b15e317f8d6dbb9d22ae0fa9
komninoschat <komninoschat@gmail.com>
2021-06-03 08:45:13 +0000

    fix: remove primary key does not exist

--- db/migrations/00002_add-provider-request.sql
+++ db/migrations/00002_add-provider-request.sql
@@ -1,7 +1,6 @@
 CREATE TABLE IF NOT EXISTS auth.provider_requests (
   id uuid NOT NULL,
   redirect_url_success text NOT NULL,
-  redirect_url_failure text NOT NULL
+  redirect_url_failure text NOT NULL,
+  PRIMARY KEY (id)
 );
-
-SELECT create_constraint_if_not_exists('auth.provider_requests', 'provider_requests_pkey', 'PRIMARY KEY (id);');
