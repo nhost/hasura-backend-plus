39639fe31c27794f6467e6f1415bd042f0a30bf3
Nuno Pato <nunopato@gmail.com>
2021-06-10 19:45:24 +0200

    Put GlobalConfig on the right place

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
+  globalSetup: '<rootDir>/src/test/global-setup.ts',
   verbose: true,
   moduleNameMapper: {
-    globalSetup: '<rootDir>/src/test/global-setup.ts',
     '^@shared/(.*)$': '<rootDir>/src/shared/$1',
     '^@test/(.*)$': '<rootDir>/src/test/$1'
   },
