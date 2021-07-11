3bbbd8f7ca29cdd54936ab168c8d53064471852c
Marcis Bergmanis <marcisbergmanis@gmail.com>
2021-07-11 11:43:17 +0300

    Update storage.ts

--- src/shared/config/storage.ts
+++ src/shared/config/storage.ts
@@ -1,14 +1,30 @@
-import { castBooleanEnv } from './utils'
+import { castBooleanEnv, envExists } from './utils'
 
 /**
  * * Storage Settings
  */
 export const STORAGE = {
   get ENABLED() {
-    return castBooleanEnv('STORAGE_ENABLED') || castBooleanEnv('STORAGE_ENABLE') || true
+    if (envExists('STORAGE_ENABLED')) {
+      return castBooleanEnv('STORAGE_ENABLED')
+    }
+
+    if (envExists('STORAGE_ENABLE')) {
+      return castBooleanEnv('STORAGE_ENABLE')
+    }
+
+    return true
   },
   get S3_SSL_ENABLED() {
-    return castBooleanEnv('S3_SSL_ENABLED') || castBooleanEnv('S3_SSL_ENABLE') || true
+    if (envExists('S3_SSL_ENABLED')) {
+      return castBooleanEnv('S3_SSL_ENABLED')
+    }
+
+    if (envExists('S3_SSL_ENABLE')) {
+      return castBooleanEnv('S3_SSL_ENABLE')
+    }
+
+    return true
   },
   get S3_BUCKET() {
     return process.env.S3_BUCKET || ''
