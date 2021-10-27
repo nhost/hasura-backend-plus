8e584c07dd30262696f96afd75b282998b630726
Josh Medeski <josh.medeski@gmail.com>
2021-10-27 15:23:13 -0500

    fix: visually sort imports to force rebuild

--- src/routes/auth/mfa/sms/index.ts
+++ src/routes/auth/mfa/sms/index.ts
@@ -1,9 +1,9 @@
 import { SMS_MFA } from '@shared/config'
 import { Router } from 'express'
 
+import generateSmsMfa from './sms-generate'
 import disableSmsMfa from './sms-disable'
 import enableSmsMfa from './sms-enable'
-import generateSmsMfa from './sms-generate'
 import resendSms from './sms-resend'
 import totpSmsMfa from './sms-totp'
 
@@ -22,8 +22,8 @@ router.use((_, res, next) => {
 export const verificationMsg = (code: string): string => `Your verification code is: ${code}`
 
 export default router
+  .post('/generate', generateSmsMfa)
   .post('/disable', disableSmsMfa)
   .post('/enable', enableSmsMfa)
-  .post('/generate', generateSmsMfa)
   .post('/resend', resendSms)
   .post('/totp', totpSmsMfa)
